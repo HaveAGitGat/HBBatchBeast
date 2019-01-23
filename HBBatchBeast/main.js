787647272d6288a950e6ff3bbad9522bd801d4a3
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-01-23 07:26:42 +0000

    Move to nodejs version
    Move to nodejs version

---
+++ HBBatchBeast/main.js
@@ -0,0 +1,149 @@
+// // squirrel cage, part of installer
+
+//  //handle setupevents as quickly as possible
+//  const setupEvents = require('./installers/setupEvents')
+//  if (setupEvents.handleSquirrelEvent()) {
+//     // squirrel event handled and app will exit in 1000ms, so don't do anything else
+//     return;
+//  }
+
+
+
+
+//require electron module
+const electron = require('electron')
+
+//core node js module
+const url = require('url');
+
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+
+//declare main window limited to in scope to this block
+let mainWindow;
+
+//SET ENV
+process.env.NODE_ENV = "production";
+
+
+
+//listen for app to be ready
+
+app.on('ready', function () {
+
+    //create new window
+
+    mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 1000,
+        title: 'HBBatchBeast'
+
+
+    });
+
+    //Load html into window
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+
+    }));
+
+    //Quit app when closed
+    mainWindow.on('closed', function () {
+        app.quit();
+
+    });
+
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+
+//Create menu template
+
+const mainMenuTemplate = [
+
+
+
+    {
+        label: 'File',
+
+        submenu: [
+
+
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+
+                click() {
+                    app.quit();
+                }
+
+            }
+
+        ]
+
+    }
+];
+
+
+
+//If mac, add empty object to menu
+
+if (process.platform == 'darwin') {
+
+    //adds on empty object '{}' to beginning of array. Need this part for Macs so 'file' shows in top right of main window
+    mainMenuTemplate.unshift({});
+
+}
+
+//Add developer tools item if not in production
+
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+
+    });
+
+
+}
+
+
+//Catch item:add
+
+ipcMain.on('item:add', function (e, item) {
+
+    //  console.log(item);
+
+    
+  
+
+
+
+
+
+
+
+});
+
+
---
+++ HBBatchBeast/worker1.js
@@ -0,0 +1,340 @@
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+
+//SET ENV
+process.env.NODE_ENV = "production";
+
+
+
+
+
+var fs = require('fs');
+
+var workerNumber = "1";
+
+var preset = fs.readFileSync("./Config/presetString.txt", 'utf8');
+
+var customPresets = fs.readFileSync("./Config/customPreset.txt", 'utf8');
+
+
+queueNumberPath = "./Config/Processes/queueNumberPath.txt";
+sourcePath = "./Config/Processes/sourceQueue.txt";
+destinationPath = "./Config/Processes/destinationQueue.txt"
+destinationPathFinal = "./Config/Processes/destinationFinalQueue.txt"
+
+
+//var handBrakeCLIPath = __dirname + "\\HandBrakeCLI.exe";
+
+
+
+if(process.platform=='win32'){
+
+    var stringProcessingSlash ="\\";
+            }
+    
+            if(process.platform=='linux'){
+                var stringProcessingSlash ="/";
+            }
+    
+
+
+var fullPath=__dirname;
+
+console.log(fullPath)
+
+//fullPath = fullPath.slice(0,fullPath.lastIndexOf('\\'));
+//fullPath = fullPath.slice(0,fullPath.lastIndexOf('\\'));
+fullPath = fullPath.slice(0,fullPath.lastIndexOf(stringProcessingSlash));
+fullPath = fullPath.slice(0,fullPath.lastIndexOf(stringProcessingSlash));
+
+var fullPath2 = fullPath+ "\\HandBrakeCLI.exe"
+
+
+console.log(fullPath2)
+
+
+
+
+
+
+if(process.platform=='win32'){
+
+    if(process.env.NODE_ENV == 'production'){
+
+        //production
+var handBrakeCLIPath = "\"" +fullPath2+"\"" ;
+
+    }else{
+
+        //development
+var handBrakeCLIPath = "\"" +  __dirname + "/HandBrakeCLI.exe\"";
+
+    }
+
+}
+
+if(process.platform=='linux'){
+    //development && //production
+var handBrakeCLIPath = "HandBrakeCLI -i \""
+
+
+}
+
+
+//check to see if bat option enabled
+if(fs.existsSync("./Config/customBatPath.txt")){
+
+    var batOnOff  = fs.readFileSync("./Config/customBatPath.txt", 'utf8');
+
+}
+
+
+var globalQueueNumber;
+var currentLineNumber = 0;
+
+
+
+var viewCMDOnOff = fs.readFileSync("./Config/viewCMDOnOff.txt", 'utf8');
+
+
+var iStreamSource = fs.readFileSync(sourcePath, 'utf8')
+iStreamSource = iStreamSource.toString().split("\n");
+var iStreamDestination = fs.readFileSync(destinationPath, 'utf8')
+iStreamDestination = iStreamDestination.toString().split("\n");
+
+var tempFolderSected = fs.readFileSync("./Config/tempDestinationOnOff.txt", 'utf8');
+
+
+
+if (tempFolderSected == "1") {
+
+var iStreamDestinationFinal = fs.readFileSync(destinationPathFinal, 'utf8');
+iStreamDestinationFinal = iStreamDestinationFinal.toString().split("\n");
+}
+
+console.log(iStreamSource.length)
+
+for (var i = 0; i < iStreamSource.length - 1; i++) {
+
+// sleep(((2000 * Math.random())));
+
+
+// 
+
+var currentSourceLine = iStreamSource[i];
+var currentDestinationLine = iStreamDestination[i];
+
+if (tempFolderSected == "1") {
+var currentDestinationFinalLine = iStreamDestinationFinal[i];
+}
+
+var  globalQueueNumber=  fs.readFileSync(queueNumberPath, 'utf8');
+
+//   console.log("Currentline:"+currentLineNumber)
+//  console.log("Global queue:"+globalQueueNumber)
+if (currentLineNumber == globalQueueNumber) {
+
+// console.log(globalQueueNumber)
+
+globalQueueNumber++;
+
+fs.writeFileSync(queueNumberPath, globalQueueNumber, 'utf8');
+
+fs.writeFileSync("./Config/Processes/WorkerStatus/Worker"+workerNumber+"FileName.txt", currentDestinationLine, 'utf8');
+
+
+fs.writeFileSync("./Config/Processes/WorkerStatus/Worker"+workerNumber+"QueueNumber.txt", globalQueueNumber, 'utf8');
+
+
+
+
+if(process.platform=='win32'){
+
+    fs.writeFileSync("./Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber +".bat",  handBrakeCLIPath + " -i \"" + currentSourceLine + "\" -o \"" + currentDestinationLine + "\" " + preset, 'utf8');
+
+    }
+    
+    if(process.platform=='linux'){
+
+        fs.writeFileSync("./Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber +".sh", "HandBrakeCLI -i \"" + currentSourceLine + "\" -o \"" + currentDestinationLine + "\" " + preset, 'utf8');        
+
+    }
+    
+
+
+
+//run bat file
+
+if(process.platform=='win32'){
+
+    if(process.env.NODE_ENV == 'production'){
+
+        //production
+var workerpath = fullPath+ "/Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber + ".bat";
+
+
+
+ }else{
+
+            //development
+var workerpath = __dirname + "/Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber + ".bat";
+
+  }
+
+
+    }
+    
+    if(process.platform=='linux'){
+
+        if(process.env.NODE_ENV == 'production'){
+
+                    //production
+//var workerpath = fullPath+ "/Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber + ".sh";
+var workerpath = __dirname + "/Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber + ".sh";
+
+        }else{
+
+            //development
+        var workerpath = __dirname + "/Config/Processes/BatchFiles/HandbrakeCLIBatchTemp" + workerNumber + ".sh";
+
+
+        }
+    }
+
+
+  //  workerpath=  workerpath.replace(/ /g, '\\');
+
+  if(process.platform=='win32'){
+
+  workerpath= "\""+ workerpath+"\"";
+
+  }
+
+
+
+
+require('child_process').execSync( workerpath , function (err, stdout, stderr) {
+if (err) {
+// Ooops.
+//console.log(stderr);
+//console.log("Error with worker number:"+workerNumber);
+return console.log(err);
+}
+
+// Done.
+//runEndSection();
+//runScan();
+console.log(stdout);
+});
+
+console.log(workerpath)
+
+
+
+console.log(batOnOff)
+    if(batOnOff != ""){
+
+      var path = batOnOff;
+      path = "\""+path+"\""
+
+      require('child_process').execSync( path , function (err, stdout, stderr) {
+        if (err) {
+        // Ooops.
+        //console.log(stderr);
+        //console.log("Error with worker number:"+workerNumber);
+        return console.log(err);
+        }
+        
+        // Done.
+        //runEndSection();
+        //runScan();
+        console.log(stdout);
+        });
+
+    }
+
+
+
+
+
+
+fs.appendFileSync("./Config/Processes/WorkerStatus/completedQueue.txt",globalQueueNumber+"\n", 'utf8');
+
+
+
+///
+var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+        today = dd + '/' + mm + '/' + yyyy;
+        today2 = dd + '-' + mm + '-' + yyyy;
+
+        var d = new Date(),
+            h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+            m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+        s = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+        timenow = h + '-' + m + '-' + s;
+
+
+
+
+
+        if (tempFolderSected == "1") {
+
+            try {
+            
+
+                fs.renameSync(currentDestinationLine, currentDestinationFinalLine)
+
+
+            } catch (err) {
+                //     fso.DeleteFile(currentDestinationLine)
+              //  sleep(((1000 * Math.random()) + 1000));
+        
+
+                fs.renameSync(currentDestinationLine, currentDestinationFinalLine)
+
+
+            }
+
+            fs.appendFileSync("./Logs/fileConversionLog.txt", today2 + "-" + timenow + "--------Converted----------" + currentSourceLine + "------------to----------" + currentDestinationFinalLine + "----------using preset----------:" + preset + "\r\n", 'utf8');
+      
+
+           
+
+        }else{
+            fs.appendFileSync("./Logs/fileConversionLog.txt", today2 + "-" + timenow + "--------Converted----------" + currentSourceLine + "------------to----------" + currentDestinationLine + "----------using preset----------:" + preset+"\r\n", 'utf8');
+        }
+
+
+        currentLineNumber++;
+
+//  if (currentLineNumber == globalQueueNumber) {
+
+
+} else{
+
+currentLineNumber++;
+}
+
+
+}//end for istreamsource length
+
+
+fs.writeFileSync("./Config/Processes/WorkerStatus/Worker" + workerNumber + "FileName.txt", "Complete!", 'utf8');
+fs.writeFileSync("./Config/Processes/WorkerStatus/Worker" + workerNumber + "QueueNumber.txt", "Complete!", 'utf8');
+
