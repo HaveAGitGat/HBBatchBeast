788f342aab7ece74349c172eaeea313d4ae08e18
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-02-16 01:38:40 +0000

    New status info

--- HBBatchBeast/worker2.js
+++ HBBatchBeast/worker2.js
@@ -8,9 +8,6 @@ console.log("shellThread")
 var workerCommand = infoArray[0];
 
 
-
-// shell.exec(workerCommand).code
-
 shell.exec(workerCommand, function(code, stdout, stderr) {
  //console.log('Exit code:', code);
 
@@ -22,21 +19,7 @@ process.send("Exit,"+code);
 
  //console.log('stderr:', stderr);
 
-// var str =""+stdout;
 
-//       if(str.includes("%")){
-//         if(str.length>=7){
-
-//         n = str.indexOf("%");
-// //console.log(str.substring(n-6,n+1));
-
-// var output = str.substring(n-6,n+1)
-
-// console.log('stdout:',output)
-
-
-      //  }
-      // }
 
 
 process.exit()
