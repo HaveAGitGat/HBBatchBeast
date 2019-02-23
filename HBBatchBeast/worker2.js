9ad02f89c7b5a68852c695067b825d55d070bf55
unknown <rrwstubbs@gmail.com>
2019-02-23 00:41:18 +0000

    Test

--- HBBatchBeast/worker2.js
+++ HBBatchBeast/worker2.js
@@ -1,96 +1,30 @@
 
-//global variables
-var shellWorker
-
-
 var shell = require('shelljs');
-
-
-
-var fs = require('fs');
- 
-if (process.platform == 'win32'||process.platform == 'linux') {
-
-var homePath = "."
-
-    
-}
-
-if (process.platform == 'darwin'){
-
-    var homePath = home
-
-}
 
 process.on('message', (infoArray) => {
 
 console.log("shellThread")
 
-
-if(infoArray[0]=="processFile"){
-
-
-var workerCommand = infoArray[1];
+var workerCommand = infoArray[0];
 
 
-shellWorker = shell.exec(workerCommand, function(code, stdout, stderr,stdin) {
+shell.exec(workerCommand, function(code, stdout, stderr) {
  //console.log('Exit code:', code);
 
 process.send("Exit,"+code);
 
 
-  //console.log('Program output:', stdout+"    PID:"+shellWorker.pid);
+ // console.log('Program output:', stdout);
 
 
  //console.log('stderr:', stderr);
 
 
- //fs.appendFileSync(homePath+"/HBBatchBeast/Config/Processes/WorkerStatus/Console/ShellLog.txt", "exit", 'utf8');
+
 
 process.exit()
 });
-}
 
-
-if(infoArray[0]=="exitThread"){
-
-    
-
-
-try{
-
-
-if(process.platform=='win32'){
-var killCommand = 'taskkill /PID '+shellWorker.pid+' /T /F'
-}
-if(process.platform=='linux'){
-//var killCommand = 'vps -o pid --no-headers --ppid ' + shellWorker.pid
-var killCommand = 'pkill ' + shellWorker.pid
-}
-if(process.platform=='darwin'){
-var killCommand = 'pgrep -P ' + shellWorker.pid
-}
-
-
- if (shell.exec(killCommand).code !== 0) {
-  shell.exit(1);
-}
-
-
-
-
-     
-    }catch(err){
-
-    }
-
-
-process.send("Exit,"+"Cancelled");
-    
-
-process.exit();
-
-}
 
 
 
